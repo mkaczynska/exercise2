dfea98d821f1bed7d368da1ee5400a0141165c23
mkaczynska <magdalena.kaczynska@blstream.com>
2016-03-03 09:35:49 +0100

    first commit

---
+++ exercise2/src/InterfaceMap.java
@@ -0,0 +1,5 @@
+/**
+ * Created by user on 03-Mar-16.
+ */
+public class InterfaceMap {
+}
---
+++ exercise2/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Madzia on 03-Mar-16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
